8bf7bd8de43a5d09bc93bcafb73c3d0454c3f370
shnCanos <catellacatellacarettacaretta@gmail.com>
2025-06-07 19:06:06 +0100

    second backup after a bit

--- src/component.rs
+++ src/component.rs
@@ -1,105 +1,36 @@
 use std::{
-    alloc::{alloc, Allocator, Global, Layout}, any::{Any, TypeId}, cell::RefCell, collections::HashMap, num::NonZero, ops::{Index, IndexMut}, ptr::NonNull, rc::Rc
+    any::TypeId,
+    collections::HashMap,
+    ops::{Deref, DerefMut},
 };
 
+use any_vec::AnyVec;
 use tinysimpleecs_rust_macros::implement_bundle;
 
 pub(crate) struct ComponentCollumn {
     /// The id used for the bitmask
     id: usize,
-    data: NonNull<[u8]>,
-    item_layout: Layout,
-    current_layout: Layout,
-    capacity: usize,
-    len: usize
+    data: AnyVec,
 }
 
 impl ComponentCollumn {
-    // I will need to check this later
-    pub(crate) fn new<C: Component>(id: usize) -> Self {
-        let layout = Layout::new::<C>();
-        assert_ne!(layout.align(), 0); // Alignment mustn't equal 0
-        Self {
-            data: unsafe {NonNull::new_unchecked(layout.align() as *mut u8)},
-            item_layout: layout,
-            current_layout: layout,
-            capacity: 0,
-            len: 0,
-            id,
-        }
+    pub(crate) fn new<C: Component + 'static>(id: usize) -> Self {
+        let data = AnyVec::new::<ComponentInserter<C>>();
+        return ComponentCollumn { id, data };
     }
+}
 
-    pub(crate) fn id(&self) -> usize {
-        return self.id;
+impl Deref for ComponentCollumn {
+    type Target = AnyVec;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
     }
+}
 
-    // -- Copied straight from bevy's BlobVec -- //
-
-    /// Returns the number of elements in the vector.
-    #[inline]
-    pub fn len(&self) -> usize {
-        self.len
-    }
-
-    /// Returns `true` if the vector contains no elements.
-    #[inline]
-    pub fn is_empty(&self) -> bool {
-        self.len == 0
-    }
-
-    /// Returns the [`Layout`] of the element type stored in the vector.
-    #[inline]
-    pub fn layout(&self) -> Layout {
-        self.item_layout
-    }
-
-    // -- // -- //
-
-    pub(crate) fn ensure_minimum(&mut self, new_capacity: usize) {
-        if new_capacity <= self.capacity {
-            return;
-        }
-        let allocator = Global;
-
-        let new_capacity = self.capacity + self.capacity.max(new_capacity);
-        let new_layout = self.layout().repeat_packed(new_capacity).unwrap();
-        let ptr = unsafe {
-            if self.capacity == 0 {
-                self.data
-            } else {
-                allocator.grow(self.data, self.current_layout, new_layout).unwrap()
-            }
-        };
-    }
-    
-    /// Pushes a raw component into the ComponentCollumn
-    ///
-    /// # Safety
-    ///
-    /// `raw_component`'s `TypeId` must match `ComponentCollumn`'s key
-    pub(crate) unsafe fn push_raw(&mut self, raw_component: *const u8) {
-        self.data.extend_from_slice(bytes);
-    }
-
-    /// Pushes a component into the ComponentCollumn
-    ///
-    /// # Safety
-    ///
-    /// `component`'s `TypeId` must match `ComponentCollumn`'s key
-    pub(crate) unsafe fn push<C: Component>(&mut self, component: C) {
-        debug_assert_eq!(Layout::new::<C>(), self.layout(), "Attempted to push value with different layout from expected");
-        let bytes =
-            std::slice::from_raw_parts(&component as *const C as *const u8, self.item_layout.size());
-        self.data.extend_from_slice(bytes);
-    }
-
-    /// Gets a component from its index in ComponentCollumn
-    ///
-    /// # Safety
-    ///
-    /// `component`'s `TypeId` must match `ComponentCollumn`'s key
-    pub(crate) fn get_index_mut<C: Component>(&mut self) -> &mut C {
-        let ptr = self.data.as_ptr().add()
+impl DerefMut for ComponentCollumn {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.data
     }
 }
 
@@ -141,6 +72,8 @@ impl ComponentManager {
         anyvec.push(component);
     }
 }
+
+pub struct ComponentInserter<C: Component>(C);
 
 pub trait Component: std::fmt::Debug {}
 
