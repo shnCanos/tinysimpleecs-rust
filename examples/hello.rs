54ec93d57429877a3472daf8ca677d89eacdec82
shnCanos <catellacatellacarettacaretta@gmail.com>
2025-07-03 21:26:10 +0100

    added despawn to hello example

--- examples/hello.rs
+++ examples/hello.rs
@@ -4,13 +4,24 @@ use tinysimpleecs_rust::{Commands, Component, Query, World};
 #[derive(Debug, Component)]
 struct MyComponent(String);
 
-fn hello_there(commands: &mut Commands, query: Query<(MyComponent,), ()>) {
-    commands.spawn((MyComponent("Hello".to_owned()),));
-    commands.spawn((MyComponent("Not Hello".to_owned()),));
+#[derive(Debug, Component)]
+struct MyFlag;
+
+fn hello_there(
+    commands: &mut Commands,
+    query: Query<(MyComponent,), ()>,
+    flag_query: Query<(MyFlag,), ()>,
+) {
+    if flag_query.results.is_empty() {
+        commands.spawn((MyComponent("Hello".to_owned()),));
+        commands.spawn((MyComponent("Not Hello".to_owned()),));
+        commands.spawn((MyFlag,));
+    }
 
     println!("Printing!");
     for result in &query.results {
         dbg!(result);
+        commands.despawn(result.entity);
     }
 }
 
@@ -19,8 +30,9 @@ fn main() {
     world.add_system(hello_there).unwrap();
 
     println!(
-        "- In the first print, nothing should appear since the commands have yet to be applied.\n- However, in the second print, the two entities that we spawned should appear"
+        "- In the first print, nothing should appear since the commands have yet to be applied.\n- However, in the second print, the two entities that we spawned should appear. In the third, nothing should appear again since we despawned the entities!"
     );
+    world.run_all_systems();
     world.run_all_systems();
     world.run_all_systems();
 }
