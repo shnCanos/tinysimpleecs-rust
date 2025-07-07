203ea0523bd41ddf04a220c76c60f40b999d2282
shnCanos <catellacatellacarettacaretta@gmail.com>
2025-07-07 17:19:08 +0100

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Tiny Simple ECS (rust version) flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              cargo
+              cargo-expand
+            ];
+
+            CARGO_TERM_COLOR = "always";
+          };
+
+          raylib = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              pkg-config
+              cmake
+              xorg.libXi
+              xorg.libX11
+              xorg.libXrandr
+              xorg.libXcursor
+              xorg.libXinerama
+              libGLU
+            ];
+            packages = with pkgs; [
+              cargo
+            ];
+
+            CARGO_TERM_COLOR = "always";
+            LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+          };
+        };
+      }
+    );
+}
