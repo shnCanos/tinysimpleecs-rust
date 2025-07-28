cc6736a248923a0ba6e0907296403d4c7f014937
shnCanos <catellacatellacarettacaretta@gmail.com>
2025-07-28 15:27:08 +0100

    added fenix overlay to flake

--- flake.nix
+++ flake.nix
@@ -4,25 +4,50 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    fenix,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [
+            (final: prev: {
+              rust-analyzer-fenix = fenix.packages.${prev.stdenv.hostPlatform.system}.rust-analyzer;
+              rustToolchain =
+                fenix.packages.${prev.stdenv.hostPlatform.system}.fromToolchainFile
+                {
+                  file = ./rust-toolchain.toml;
+                  sha256 = "sha256-O8q4Dwx8yWkK2BsA+cztPBKSUSyBl7gnS27YTNPGuaY=";
+                };
+            })
+          ];
+        };
       in {
         devShells = {
           default = pkgs.mkShell {
             packages = with pkgs; [
-              rustup
+              rustToolchain
               cargo-expand
+              rust-analyzer-fenix
             ];
 
             CARGO_TERM_COLOR = "always";
+            env = {
+              # Required by rust-analyzer
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+            };
           };
 
           raylib = pkgs.mkShell {
@@ -37,11 +62,15 @@
               libGLU
             ];
             packages = with pkgs; [
-              rustup
+              rustToolchain
+              cargo-expand
+              rust-analyzer-fenix
             ];
-
-            CARGO_TERM_COLOR = "always";
-            LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+            env = {
+              # Required by rust-analyzer
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+              LIBCLANG_PATH = "${pkgs.llvmPackages_16.libclang.lib}/lib";
+            };
           };
         };
       }
